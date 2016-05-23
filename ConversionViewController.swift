97c3d4679cf57c8d2e4fb80fee1c5fc95989fec8
Liz Borchardt <zardra@gmail.com>
2016-05-23 16:46:31 -0700

    Converts entered fahrenheit numbers to celsius

---
+++ ConversionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ConversionViewController.swift
+//  World-Trotter
+//
+//  Created by Liz Borchardt on 5/23/16.
+//  Copyright © 2016 Elizabeth Borchardt. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+}
